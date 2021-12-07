1668c06c086e4abbcacfd20f9f88316eaa18ad36
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-06 18:20:50 -0600

    added model function calls to all routes

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,36 +2,50 @@ const router = require('express').Router()
 
 const Accounts = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
+    const accounts = await Accounts.getAll()
+    res.json(accounts)
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:id', (req, res, next) => {
+// TODO: checkId middleware
+router.get('/:id', async (req, res, next) => {
   try {
+    const account = await Accounts.getById(req.params.id)
+    res.json(account)
   } catch (err) {
     next(err)
   }
 })
 
-router.post('/', (req, res, next) => {
+// TODO: checkAccount middleware
+router.post('/', async (req, res, next) => {
   try {
+    const newAccount = await Accounts.create(req.body)
+    res.status(201).json(newAccount)
   } catch (err) {
     next(err)
   }
 })
 
-router.put('/:id', (req, res, next) => {
+// TODO: checkId and checkAccount middleware
+router.put('/:id', async (req, res, next) => {
   try {
+    const updatedAccount = await Accounts.updateById(req.params.id, req.body)
+    res.status(201).json(updatedAccount)
   } catch (err) {
     next(err)
   }
 })
 
-router.delete('/:id', (req, res, next) => {
+// TODO: checkId middleware
+router.delete('/:id', async (req, res, next) => {
   try {
+    const deletedAccount = await Accounts.deleteById(req.params.id)
+    res.json(deletedAccount)
   } catch (err) {
     next(err)
   }
