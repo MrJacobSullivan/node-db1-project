376ac5839b18f4301078a3eef5b32cfad4f91a0c
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-06 19:53:29 -0600

    added checkAccountPayload function, all tests passing

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,20 +1,30 @@
 const Accounts = require('./accounts-model')
 
 const checkAccountPayload = (req, res, next) => {
-  if (!req.body.name || !req.body.budget) {
-    next({ status: 400, message: '' })
+  const { name, budget } = req.body
+
+  if (name === undefined || budget === undefined) {
+    next({ status: 400, message: 'name and budget are required' })
+  } else if (typeof name !== 'string') {
+    next({ status: 400, message: 'name of account must be a string' })
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    next({ status: 400, message: 'name of account must be between 3 and 100' })
+  } else if (typeof budget !== 'number') {
+    next({ status: 400, message: 'budget of account must be a number' })
+  } else if (budget < 0 || budget > 1_000_000) {
+    next({ status: 400, message: 'budget of account is too large or too small' })
   } else {
-    req.account = { name: req.body.name.trim(), budget: req.body.budget }
+    req.account = { name: name.trim(), budget }
     next()
   }
 }
 
 const checkAccountNameUnique = async (req, res, next) => {
   const accounts = await Accounts.getAll()
-  const matchedAccount = accounts.find((account) => account.name === req.body.name)
+  const matchedAccount = accounts.find((account) => account.name === req.body.name.trim())
 
   if (matchedAccount) {
-    next({ status: 400, message: '' })
+    next({ status: 400, message: 'that name is taken' })
   } else {
     next()
   }
